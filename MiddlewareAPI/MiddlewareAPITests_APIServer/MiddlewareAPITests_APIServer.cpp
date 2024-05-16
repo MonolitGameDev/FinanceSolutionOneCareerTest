c08af533ed257268f629812d585b8895a578abc8
Ivanplat <EventGraphDev@gmail.com>
2024-05-16 18:20:46 +0300

    MiddlewareAPI Initial

---
+++ MiddlewareAPI/MiddlewareAPITests_APIServer/MiddlewareAPITests_APIServer.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MiddlewareAPITestsAPIServer
+{
+	TEST_CLASS(MiddlewareAPITestsAPIServer)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
